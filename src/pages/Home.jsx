bd3a12f4b22232ba0d57a2c1a10db5d401b0cb4c
Haider Ali <malikhaider48008@gmail.com>
2026-09-05 01:46:17 +0500

    Fix Home page navigation buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import Card from '../components/Card'
 import Button from '../components/Button'
 import Reveal from '../components/Reveal'
 import '../styles/Home.css'
+import { Link } from 'react-router-dom'
 
 
 const FLEET_SLIDES = [
